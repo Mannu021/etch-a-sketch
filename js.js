459a021f875d69b15bcc945c202e20ed642b3825
Mannu021 <mayanksaini021@gmail.com>
2021-08-13 10:46:46 +0530

    update

--- js.js
+++
@@ -1,8 +0,0 @@
-let container=document.getElementById("gride");
-let block=function(mclass){
-    return `<div id=${mclass}></div>`;
-}
-let gridSize=window.prompt("size of grid",10);
-for(let i=0;i<gridSize*gridSize;i++){
-    container.innerHTML=block("grid");
-}
